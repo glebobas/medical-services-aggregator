0ccb968198733433a1758e48d637b6a4f0509081
OEM Configuration (temporary user) <slavpolichev@gmail.com>
2023-03-14 18:53:10 +0100

    markup finished

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
-import {Register} from "../components/Modal/Register";
-import {Layout} from "./Layout";
+import { Register } from "../components/Modal/Register";
+import { Layout } from "./Layout";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout />} />
       {/*TODO: Прописать внутренние роуты*/}
-      </Route>
     </Routes>
   );
 }
