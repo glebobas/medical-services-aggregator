a1c7b5d1fefbac1509eb485d03708de439be0515
Dmitriy Vyaznikov <dmitri.vyaznikov@gmail.com>
2023-03-14 16:06:41 +0700

    dev

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Register} from "../components/Modal/Register";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Medical service aggregator</h1>
+        <Register/>
     </div>
   );
 }
