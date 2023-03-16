f9869fa63a968809882833a74c3fdb3c2b2cdf43
OEM Configuration (temporary user) <slavpolichev@gmail.com>
2023-03-16 10:41:36 +0100

    logout finished

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -71,7 +71,8 @@ export function NavBar() {
   }
 
   const logOut = () => {
-    dispatch({ type: Types.LOGOUT })
+    dispatch({ type: Types.LOGOUT });
+    localStorage.clear();
   }
 
   return (
@@ -251,33 +252,30 @@ export function NavBar() {
                             className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <Menu.Item>
                               {({active}) => (
-                                <a
-                                  href="#"
-                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                <div
+                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer')}
                                 >
                                   Профиль
-                                </a>
+                                </div>
                               )}
                             </Menu.Item>
                             <Menu.Item>
                               {({active}) => (
-                                <a
-                                  href="#"
-                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                <div
+                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer')}
                                 >
                                   Редактирование
-                                </a>
+                                </div>
                               )}
                             </Menu.Item>
                             <Menu.Item>
                               {({active}) => (
-                                <a
+                                <div
                                 onClick={logOut}
-                                  href="#"
-                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer')}
                                 >
                                   Выход
-                                </a>
+                                </div>
                               )}
                             </Menu.Item>
                           </Menu.Items>
