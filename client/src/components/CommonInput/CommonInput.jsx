6e01b06ee5ddb3a8a74bcdd68f065dd4467a01fc
Dmitriy <d.kovalenko174@gmail.com>
2023-03-17 16:38:01 +0500

    Fixed input
    Работает ниспадающее меню

--- client/src/components/CommonInput/CommonInput.jsx
+++ client/src/components/CommonInput/CommonInput.jsx
@@ -1,75 +1,57 @@
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useRef, useState} from "react";
+import {useSelector} from "react-redux";
 import {Select} from 'antd';
-
-import { Types } from "../../redux/types/types";
-
-const OPTIONS = ['Иванов Иван Иванович', 'Петров Петр Петрович', 'Сидоров Сидор Сидорович', 'Клиника Беркут'];
 
 export function CommonInput() {
 
   const getClinicsAndDoctors = useSelector((state) => state?.clinicsAndDoctors?.clinicsAndDoctors?.map(item => item.name))
+  console.log("-> getClinicsAndDoctors", getClinicsAndDoctors);
 
-  // Выпадающее меню
-  const [originalList, setOriginalList] = useState(getClinicsAndDoctors)
-  const [filteredList, setFilteredList] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const wrapperRef = useRef(null);
 
-  const handleSearchInput = (event) => {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setResults([]);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [wrapperRef]);
 
-    setSearchInput(event.target.value);
-    //
-    //   fetch(`main/alldata/${event.target.value}`)
-    //     .then(response => response.json())
-    //     .then(data => [...data.readyClinicList, ...data.readyDoctorList])
-    //     .then(data => {
-    //       const newArray = data.map(item => item.name);
-    //       setFilteredList(newArray)
-    //     })
-    //     .catch(error => {
-    //       console.error(error);
-    //     })
-    console.log("-> event", event.target.value);
-  }
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    const filteredResults = getClinicsAndDoctors.filter((item) =>
+      item.toLowerCase().includes(value.toLowerCase())
+    );
+    const limitedResults = filteredResults.slice(0, 6);
+    setResults(limitedResults);
+  };
 
   return (
-    <div className="flex flex-col w-full mx-auto">
-      <input list="options" className="rounded-lg border py-1 px-5" type="text" value={searchInput}
-             onChange={handleSearchInput}/>
-      <datalist id="options" className="w-[420px]">
-        {originalList?.map((option) => (
-          <option key={option.id} value={option}/>
-        ))}
-      </datalist>
-      {/*<ul>*/}
-      {/*  {filteredList.map(item => (<li key={item.id}>{item.name}</li>))}*/}
-      {/*</ul>*/}
+    <div className="relative w-full" ref={wrapperRef}>
+      <input
+        type="text"
+        placeholder="Search"
+        className="px-4 py-2 w-full border border-gray-300 rounded-md"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      {results.length > 0 && (
+        <ul className="absolute w-full bg-white border border-gray-300 rounded-md mt-1 z-10">
+          {results.map((item) => (
+            <li key={item} className="px-4 py-2">
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-
-// OLD CODE
-
-    // const [selectedItems, setSelectedItems] = useState([]);
-    // const filteredOptions = OPTIONS.filter((o) => !selectedItems.includes(o));
-    //
-    // return (
-    //   <div className="flex flex-col w-full mx-auto">
-    //     <label htmlFor="request" className="flex text-lg font-bold leading-20 dark:text-blue-100">
-    //        Найти клинику или врача
-    //     </label>
-    //     <div className=" mt-2 rounded-md shadow-sm">
-    //       <Select
-    //         mode="multiple"
-    //         placeholder="Найти врача | Клинику | Записаться на прием"
-    //         value={selectedItems}
-    //         onChange={setSelectedItems}
-    //         style={{ width: '100%' }}
-    //         options={filteredOptions.map((item) => ({
-    //           value: item,
-    //           label: item,
-    //         }))}
-    //       />
-    //
-    //     </div>
-    //   </div>
   )
+
 }
