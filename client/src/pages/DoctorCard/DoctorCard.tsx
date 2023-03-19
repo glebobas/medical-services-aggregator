d3773a7e4a707b2ef8497349c01f2b8af32b526d
Dmitriy <d.kovalenko174@gmail.com>
2023-03-20 00:09:21 +0500

    Added date into DoctorCard

--- client/src/pages/DoctorCard/DoctorCard.tsx
+++ client/src/pages/DoctorCard/DoctorCard.tsx
@@ -12,7 +12,7 @@ export const DoctorCard = () => {
   useEffect(() => {
     fetch(`/main/doctor/${id}`)
       .then(response => response.json())
-      .then(response => console.log(response))
+      .then(response => setDoctor(response.readyDocOne))
       .catch(err => console.error(err))
   }, [])
 
@@ -20,21 +20,21 @@ export const DoctorCard = () => {
     <div className="doctor__cad flex flex-col w-full mx-auto border rounded py-4 px-6 mt-4 shadow">
       <div className="doctor__card-row-1 flex flex-row justify-between">
         <div className="row-1__column-left flex flex-col justify-between">
-          <div className="doctor__card-title text-xl font-semibold">Иванов Иван Иванович</div>
+          <div className="doctor__card-title text-xl font-semibold">{doctor.name}</div>
           <div className="params gap-2">
-            <div className="doctor__card-special text-sm text-gray-500">врач-терапевт</div>
+            <div className="doctor__card-special text-sm text-gray-500">{doctor.speciality}</div>
             <div className="doctor__card-experience text-sm text-gray-500">стаж: <span
-              className="font-semibold">5 лет</span></div>
-            <div className="doctor__card-rate text-sm text-gray-500">рейтинг: <span className="font-semibold">4.8 / 5</span></div>
-            <div className="doctor__card-row-2-clinic text-sm text-gray-500">Клиника: <span className="font-semibold">Беркут</span></div>
+              className="font-semibold">-</span></div>
+            <div className="doctor__card-rate text-sm text-gray-500">рейтинг: <span className="font-semibold">{doctor.averageDocRating}</span></div>
+            <div className="doctor__card-row-2-clinic text-sm text-gray-500">Клиника: <span className="font-semibold">{doctor.clinic}</span></div>
             <div className="doctor__card-row-2-clinic-address text-sm text-gray-500">Адрес: <span className="font-semibold">г.СПБ, Выборгское ш., 17 </span></div>
           </div>
         </div>
-        <div className="row-1__column-right flex border w-[150px] h-[150px] rounded bg-blue-200 items-center justify-center shadow">foto</div>
+        <div className="row-1__column-right flex border w-[150px] h-[150px] rounded bg-blue-200 items-center justify-center shadow"><img src={doctor.avatar} /></div>
       </div>
       <div className="doctor__card-row-2 mt-6">
 
-        <div className="doctor__card-row-2-doctor-params text-sm text-gray-500 mt-4">О враче: <span className="font-semibold">выполняет все виды помощи по специальности “Терапия”. Имеет соответствующие сертификаты </span></div>
+        <div className="doctor__card-row-2-doctor-params text-sm text-gray-500 mt-4">О враче: <span className="font-semibold">{doctor.generalInfo}</span></div>
       </div>
       <DayView />
     </div>
