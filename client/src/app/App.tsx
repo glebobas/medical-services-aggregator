56bca95292f7d460a818a1e36aff2f1c99f506f1
Dmitriy <d.kovalenko174@gmail.com>
2023-03-19 11:34:11 +0500

    Fixed bug login

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,8 +1,8 @@
 //@ts-nocheck
-import React, { useEffect } from 'react';
-import { Route, Routes } from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
-import { Layout } from "./Layout";
+import {Layout} from "./Layout";
 import ClinicalCard from '../pages/ClinicalCard/ClinicalCard';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
@@ -17,6 +17,8 @@ import {ListPage} from "../pages/ListPage";
 
 function App() {
 
+  const dispatch = useDispatch()
+
   useEffect(() => {
     fetch('/main/alldataquery')
       .then(response => response.json())
@@ -29,33 +31,35 @@ function App() {
       })
   }, [])
 
-  const user = useSelector(state => state.login.user.id);
+  const user = useSelector(state => state.login?.user?.id);
   console.log(!!user);
 
   return (
     <AuthProvider>
-      {!user 
-      ?
-      <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path='/error' element={<ErrorPage />} />
-        <Route path='*' element={<ErrorPage />} />
-      </Route>
-    </Routes>
-      :
-      <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path='/clients' element={<h1>Страница клиентов</h1>} />
-        <Route path='/doctors' element={<h1>Страница врачей</h1>}></Route>
-        <Route path='/calendar' element={<h1>Календарь</h1>} />
-        <Route path='/profileEditing' element={<ProfileEditing />} />
-        <Route path='/clinical' element={<ClinicalCard />} />
-        <Route path='/error' element={<ErrorPage />} />
-        <Route path='*' element={<ErrorPage />} />
-      </Route>
-    </Routes>
+      {!user
+        ?
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<MainPage/>}/>
+            <Route path='/listpage' element={<ListPage/>}/>
+            <Route path='/error' element={<ErrorPage/>}/>
+            <Route path='*' element={<ErrorPage/>}/>
+          </Route>
+        </Routes>
+        :
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<MainPage/>}/>
+            <Route path='/clients' element={<h1>Страница клиентов</h1>}/>
+            <Route path='/doctors' element={<h1>Страница врачей</h1>}></Route>
+            <Route path='/calendar' element={<h1>Календарь</h1>}/>
+            <Route path='/listpage' element={<ListPage/>}/>
+            <Route path='/profileEditing' element={<ProfileEditing/>}/>
+            <Route path='/clinical' element={<ClinicalCard/>}/>
+            <Route path='/error' element={<ErrorPage/>}/>
+            <Route path='*' element={<ErrorPage/>}/>
+          </Route>
+        </Routes>
       }
 
     </AuthProvider>
